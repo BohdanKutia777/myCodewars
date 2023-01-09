56187889effcf7046e877e445ed790690aa50fc9
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-01-09 19:45:22 +0200

    done task Sorted? yes? no? how?

--- scripts/Rotate for a Max.js
+++ scripts/Rotate for a Max.js
@@ -1,6 +1,6 @@
 const maxRot = n => {
-    let str = n.toString();
-    let arr = [str];
+let str = n.toString();
+let arr = [str];
     for (let i = 0;i<=str.length-1 ;i++){
       str = str.slice(0,i)+str.slice(i+1)+str[i];
       arr.push(str.split().join());
---
+++ scripts/Sorted yes no how.js
@@ -0,0 +1,3 @@
+const isSortedAndHow = arr => arr.every((x,i) => i == 0 ||arr[i] >= arr[i-1])?'yes, ascending':
+         arr.every((x,i) => i == 0 || arr[i] <= arr[i-1])?'yes, descending':'no'
+         
