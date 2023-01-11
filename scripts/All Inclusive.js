3bf5f0bbcf55274948eaab9488cf760bce1d94ec
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-01-11 21:40:16 +0200

    done task All Inclusive?

---
+++ scripts/All Inclusive.js
@@ -0,0 +1,9 @@
+const containAllRots = (strng, arr) => {
+    for (var i = 0; i < strng.length; i++) {
+      if (arr.indexOf(strng.slice(i) + strng.slice(0, i)) === -1) {
+        return false
+      }
+    }
+    return true
+  }
+  
