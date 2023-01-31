e4ca366560a575e0b2438a7b501598f0133323ff
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-01-31 12:35:51 +0200

    done task Letterbox Paint-Squad

---
+++ scripts/Letterbox Paint-Squad.js
@@ -0,0 +1,10 @@
+const paintLetterboxes = (start, end) => {
+let res = Array(10).fill(0);
+for (let i = start; i <= end; i++) {
+    for (let n of (i + '')) {
+        res[n]++;
+        }
+    }
+    return res;
+}
+  
