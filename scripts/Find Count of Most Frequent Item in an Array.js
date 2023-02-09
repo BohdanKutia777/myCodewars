4cb32214892dea320eb5669955e0f0b717aa36d4
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-02-09 10:40:00 +0200

    done task Find Count of Most Frequent Item in an Array

---
+++ scripts/Find Count of Most Frequent Item in an Array.js
@@ -0,0 +1,6 @@
+function mostFrequentItemCount(collection) {
+if (collection.length === 0) return 0;
+let count = Object.create(null);  
+collection.forEach( item => {count[item] = (count[item] || 0) + 1;});
+    return Math.max(...Object.values(count));
+}
