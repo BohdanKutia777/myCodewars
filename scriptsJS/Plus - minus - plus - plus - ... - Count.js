2b1fe37a1b69e091ea0f0d49de3c05228f6964d5
BohdanKutia777 <87551218+BohdanKutia777@users.noreply.github.com>
2023-03-22 21:31:50 +0200

    done task Plus - minus - plus - plus - ... - Count

---
+++ scriptsJS/Plus - minus - plus - plus - ... - Count.js
@@ -0,0 +1,8 @@
+const catchSignChange = (arr) => {
+  let count = 0;
+  let a = arr.map((v, i, arr) =>
+    (v >= 0 && arr[i + 1] < 0) || (v < 0 && arr[i + 1] >= 0) ? count++ : count
+  );
+
+  return count;
+};
